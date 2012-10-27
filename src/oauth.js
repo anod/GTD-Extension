455ad7d539209184a999b61792506415a6fc9d4c
anod <alex.gavrishev@gmail.com>
2012-10-27 13:26:01 +0200

    orginize folder structure + Gmail OAuth

---
+++ src/oauth.js
@@ -0,0 +1,11 @@
+
+ var oauth = ChromeExOAuth.initBackgroundPage({
+ 	'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+	'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+	'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+	'consumer_key': 'anonymous',
+	'consumer_secret': 'anonymous',
+	'scope': 'https://mail.google.com/',
+	'app_name': 'GTD GMail Extension',
+	'callback_page': '../vendors/oauth/chrome_ex_oauth.html'
+});
---
+++ src/popup.js
@@ -0,0 +1,18 @@
+
+
+/*
+bgPage.oauth.authorize(function(token, secret) {
+	document.getElementById("data").innerHTML = token + " : "+secret;
+});
+*/
+var bgPage = chrome.extension.getBackgroundPage();
+var $ = bgPage.$;
+var Backbone = bgPage.Backbone;
+var _ = bgPage._;
+
+$(function(){
+	bgPage.oauth.authorize(function(token, secret) {
+		console.log("Authorize - Token:"+token+", Secret:"+secret);
+	});
+	console.log("onload");
+});
