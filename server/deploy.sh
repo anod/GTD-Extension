94aca000fd29e60abbb78ad6e0dd720f8aa51208
anod <alex.gavrishev@gmail.com>
2012-11-03 14:49:42 +0200

    Update deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 ## https://github.com/resmo/git-ftp
-git ftp push -u monos108 -p - ftp://anodsplace.info/gtd
+##
+## Init: 
+##   ftp init -u monos108 -p - ftp://anodsplace.info/gtd --syncroot server
+##
+## Set scope:
+##   git config git-ftp.gtd.url ftp://anodsplace.info/gtd
+##   git config git-ftp.gtd.user monos108
+##   git config git-ftp.gtd.password <PASWORD>
+##   git config git-ftp.gtd.syncroot server
+##
+git ftp push -s gtd
