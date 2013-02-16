709d9005811aaa7d81d9b29f60fe5442f13e60f2
anod <alex.gavrishev@gmail.com>
2013-02-16 20:29:43 +0200

    remove phptail

--- server/extapi_show.php
+++ server/extapi_show.php
@@ -1,16 +1,6 @@
 <?php
 $loader = require_once 'vendor/autoload.php';
 
-$tail = new PHPTail(__DIR__ . '/'. \GTD\ExtApi::PATH);
+header("Content-type: text/plain");
 
-/**
- * We're getting an AJAX call
- */
-if(isset($_GET['ajax']))  {
-	echo $tail->getNewLines($_GET['lastsize'], $_GET['grep'], $_GET['invert']);
-	exit();
-}
-/**
- * Regular GET/POST call, print out the GUI
- */
-$tail->generateGUI('/extapi_show.php');
+echo file_get_contents(__DIR__ . '/'. \GTD\ExtApi::PATH);
