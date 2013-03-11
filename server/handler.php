628c48c1f5fe3c0d8f4732446ae97f6c715182f5
Alex Gavrishev <alex.gavrishev@gmail.com>
2013-03-11 20:08:01 +0200

    read from request

--- server/handler.php
+++ server/handler.php
@@ -2,6 +2,6 @@
 $loader = require_once 'vendor/autoload.php';
 
 $c = new \GTD\Controller();
-$response = $c->run($_POST);
+$response = $c->run($_REQUEST);
 
 echo json_encode($response->jsonSerialize());
