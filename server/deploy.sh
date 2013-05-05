c299d26bee93e2ae634d169a626badbb4f9bf528
anod <alex.gavrishev@gmail.com>
2013-05-05 08:01:06 +0300

    fix path

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,7 +6,7 @@
 ##   git ftp init -u alex -p - ftp://anodsplace-do//www/anodsplace.info/gtd --syncroot server -v
 ##
 ## Set scope:
-##   git config git-ftp.gtd.url ftp://anodsplace-do/www/anodsplace.info/gtd
+##   git config git-ftp.gtd.url ftp://anodsplace-do//www/anodsplace.info/gtd
 ##   git config git-ftp.gtd.user alex
 ##   git config git-ftp.gtd.password <PASWORD>
 ##   git config git-ftp.gtd.syncroot server
